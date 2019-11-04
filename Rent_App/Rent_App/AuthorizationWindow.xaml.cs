d2d8932d2599eda5128f377372ee2a91154a2d5d
Teegroon-on <54004088+Teegroon-on@users.noreply.github.com>
2019-11-04 13:15:46 +0300

    Authorization for one users (Full)

--- Rent_App/Rent_App/AuthorizationWindow.xaml.cs
+++ Rent_App/Rent_App/AuthorizationWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,7 +10,6 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -30,73 +30,182 @@ namespace Rent_App
 		{
 			EntryCol++;
 			bool Access = true;
-			if (LoginTB.Text != "" && LoginTB.Text != " ")
+			bool CapchaAccess = false;
+			if (LoginTB.Text != "" && LoginTB.Text != " " &&
+				PassworB.Password != "" && PassworB.Password != " ")
 			{
-				DataModel.STAFF StaffLogin = null;
-				try
+				if (EntryCol > 2)
 				{
-					var Context = new DataModel.RentContext();
-					StaffLogin = Context.STAFF.FirstOrDefault(p => p.Email == LoginTB.Text);
-				}
-				catch(Exception ex)
-				{
-					MessageBox.Show("Ошибка базы данных. Обратитесь к системному администратору.",
-						"Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-					Access = false;
-				}
-
-				if(StaffLogin.Email != null )
-				{
-					if(StaffLogin.Password == PassworB.Password)
+					if (CapchaImage.Text == CapchaTB.Text)
 					{
-						switch(StaffLogin.Role_id)
+						CapchaAccess = true;
+					}
+					if (CapchaAccess)
+					{
+						DataModel.STAFF StaffLogin = null;
+						try
 						{
-							case 2:
+							var Context = new DataModel.RentContext();
+							StaffLogin = Context.STAFF.FirstOrDefault(p => p.Email == LoginTB.Text.ToLower());
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show("Ошибка базы данных. Обратитесь к системному администратору.",
+								"Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+							Access = false;
+						}
+
+						if (StaffLogin.Email != null)
+						{
+							if (StaffLogin.Password == PassworB.Password)
+							{
+								switch (StaffLogin.Role_id)
 								{
-									Menager_C MC = new Menager_C();
-									MC.Show();
-									this.Hide();
-									break;
+									case 2:
+										{
+											Menager_C MC = new Menager_C();
+											MC.Show();
+											this.Hide();
+											break;
+										}
+									default:
+										{
+											MessageBox.Show("Доступ ограничен, для получения доступа обратитесь к администратору системы.", ""
+															, MessageBoxButton.OK, MessageBoxImage.Information);
+											Access = false;
+											break;
+										}
 								}
-							default:
-								{
-									MessageBox.Show("Доступ ограничен, для получения доступа обратитесь к администратору системы.", ""
-													, MessageBoxButton.OK, MessageBoxImage.Information);
-									Access = false;
-									break;
-								}
+							}
+							else
+							{
+								MessageBox.Show("Неверный пароль пользователя.", ""
+																, MessageBoxButton.OK, MessageBoxImage.Information);
+								Access = false;
+							}
+						}
+						else
+						{
+							MessageBox.Show("Пользователя с таким Email нет в базе данных.", ""
+								, MessageBoxButton.OK, MessageBoxImage.Information);
+							Access = false;
 						}
 					}
 					else
 					{
-						MessageBox.Show("Неверный пароль пользователя.", ""
-							                            , MessageBoxButton.OK, MessageBoxImage.Information);
+						MessageBox.Show("Неверное введен текст Капчи!", "Ошибка!",
+										MessageBoxButton.OK, MessageBoxImage.Error);
 						Access = false;
+
 					}
 				}
 				else
 				{
-					MessageBox.Show("Пользователя с таким Email нет в базе данных.", ""
-						, MessageBoxButton.OK, MessageBoxImage.Information);
-					Access = false;
+					DataModel.STAFF StaffLogin = null;
+					try
+					{
+						var Context = new DataModel.RentContext();
+						StaffLogin = Context.STAFF.FirstOrDefault(p => p.Email == LoginTB.Text);
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show("Ошибка базы данных. Обратитесь к системному администратору.",
+							"Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+						Access = false;
+					}
+
+					if (StaffLogin.Email != null)
+					{
+						if (StaffLogin.Password == PassworB.Password)
+						{
+							switch (StaffLogin.Role_id)
+							{
+								case 2:
+									{
+										Menager_C MC = new Menager_C();
+										MC.Show();
+										this.Hide();
+										break;
+									}
+								default:
+									{
+										MessageBox.Show("Доступ ограничен, для получения доступа обратитесь к администратору системы.", ""
+														, MessageBoxButton.OK, MessageBoxImage.Information);
+										Access = false;
+										break;
+									}
+							}
+						}
+						else
+						{
+							MessageBox.Show("Неверный пароль пользователя.", ""
+															, MessageBoxButton.OK, MessageBoxImage.Information);
+							Access = false;
+						}
+					}
+					else
+					{
+						MessageBox.Show("Пользователя с таким Email нет в базе данных.", ""
+							, MessageBoxButton.OK, MessageBoxImage.Information);
+						Access = false;
+					}
 				}
 			}
 			else
 			{
-				MessageBox.Show("Введите Email.", ""
-					, MessageBoxButton.OK, MessageBoxImage.Information);
+				MessageBox.Show("Для успешной авторизации, необходимо ввести и логин, и пароль.", ""
+				, MessageBoxButton.OK, MessageBoxImage.Information);
 				Access = false;
 			}
 
-			if(!Access && EntryCol>)
+			if(!Access && EntryCol> 2)
 			{
-
+				CapchaImage.Visibility = Visibility.Visible;
+				CapchaTB.Visibility = Visibility.Visible;
+				CapchaRefresh();
+				
 			}
 		}
 
 		private void CancleButClick(object sender, RoutedEventArgs e)
 		{
 			Environment.Exit(0);
+		}
+
+		private void CapchaRefresh()
+		{
+			String allowchar = "";
+
+			allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+
+			allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
+
+			allowchar += "1,2,3,4,5,6,7,8,9,0";
+
+			char[] a = { ',' };
+
+			String[] ar = allowchar.Split(a);
+
+			String pwd = "";
+
+			string temp = "";
+
+			Random r = new Random();
+
+
+
+			for (int i = 0; i < 6; i++)
+
+			{
+
+				temp = ar[(r.Next(0, ar.Length))];
+
+
+
+				pwd += temp;
+
+			}
+			CapchaImage.Text = pwd;
 		}
 	}
 }
